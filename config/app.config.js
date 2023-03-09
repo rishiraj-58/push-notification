f8d74eda4d83392f60fc05b9b0e879be8201674f
rishiraj-58 <s.rishiraj2002@gmail.com>
2023-03-09 23:05:49 +0530

    added code

---
+++ config/app.config.js
@@ -0,0 +1,8 @@
+const ONE_SIGNAL_CONFIG = {
+    APP_ID: "aa0b2f82-f16a-4b15-b6ba-e0221d7e2a61",
+    API_KEY: "OGQ4OGY0NGMtMGFjOS00NzRiLWE1NWEtMzc2YjBiNTcyNzhk"
+}
+
+module.exports = {
+    ONE_SIGNAL_CONFIG,
+}
